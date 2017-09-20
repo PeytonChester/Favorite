79ef9ebd7cf3ba1706b1ea19991df2a60f6e934b
PeytonChester <peytonchester1@gmail.com>
2017-09-20 17:00:07 -0400

    Dynamic List / Working Buttons
    In this update I made the cards dynamic, and I added a click listener to the favorite and save buttons. The listeners make it so that the icon goes from just an outline to a solid icon.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,63 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //Cards
+  cards = [
+    {
+      imgPath: "cat1.jpg",
+      title: "Orange Kitten",
+      desc: "This is a cute little orange kitten.",
+      favorited: false,
+      saved: false
+    },
+
+    {
+      imgPath: "cat2.jpg",
+      title: "Kitten In a Tube",
+      desc: "This little cat is stuck in a tube.",
+      favorited: false,
+      saved: false
+    },
+
+    {
+      imgPath: "cat3.jpg",
+      title: "White Kitten",
+      desc: "This is a cute little white kitten.",
+      favorited: false,
+      saved: false
+    }
+  ];
+
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
+  //Type Here
+  clickFav(card) {
+    let index = this.cards.indexOf(card);
+
+    if (index > -1){
+      if(card.favorited) {
+        this.cards[index].favorited = false;
+      } else {
+        this.cards[index].favorited = true;
+      }
+    }
+  }
+
+  clickSave(card) {
+    let index = this.cards.indexOf(card);
+
+    if (index > -1){
+      if(card.saved) {
+        this.cards[index].saved = false;
+      } else {
+        this.cards[index].saved = true;
+      }
+    }
+  }
+
 
 }
